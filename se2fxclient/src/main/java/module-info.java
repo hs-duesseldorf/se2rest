1b92b94bef2eb1a0bd90c0a4d18fe9d6e60ebd91
Feichtmeier <frederik.feichtmeier@gmail.com>
2020-09-08 15:53:06 +0200

    Add module-info

---
+++ se2fxclient/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.hsd.inflab.se2fxclient {
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.json;
+
+    opens org.hsd.inflab.se2fxclient.view to javafx.fxml;
+    opens org.hsd.inflab.se2fxclient.controller to javafx.fxml;
+    exports org.hsd.inflab.se2fxclient.controller;
+    exports org.hsd.inflab.se2fxclient.view;
+    exports org.hsd.inflab.se2fxclient.model;
+}
