ad8ce7f797f8e0356d03417b83ba64edbc2f9043
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-03-24 10:29:08 +0100

    remove src

--- se2server/src/main/java/org/hsd/inflab/se2server/repository/GenericRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hsd.inflab.se2server.repository;
-
-import org.hsd.inflab.se2server.entity.AbstractEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public abstract interface GenericRepository<T extends AbstractEntity> extends JpaRepository<T, Long> {
-
-}
--- se2server/src/main/java/org/hsd/inflab/se2server/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hsd.inflab.se2server.repository;
-
-import org.hsd.inflab.se2server.entity.Person;
-
-public interface PersonRepository extends GenericRepository<Person> {
-    
-}
